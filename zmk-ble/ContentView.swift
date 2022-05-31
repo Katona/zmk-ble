ec796c90b13c6a2c9b35aab9e3268d5a54008244
Gabor Hornyak <gabhornyak@gmail.com>
2022-05-31 21:17:25 +0200

    Initial Commit

---
+++ zmk-ble/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  zmk-ble
+//
+//  Created by Gabor Hornyak on 2022. 05. 31..
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ zmk-ble/zmk_bleApp.swift
@@ -0,0 +1,17 @@
+//
+//  zmk_bleApp.swift
+//  zmk-ble
+//
+//  Created by Gabor Hornyak on 2022. 05. 31..
+//
+
+import SwiftUI
+
+@main
+struct zmk_bleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
